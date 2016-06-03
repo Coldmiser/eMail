c0ff5ace22f843b91d42851ea5c42963360a4a18
Coldmiser <coldmiser487@gmail.com>
2016-06-03 13:45:10 -0500

    Initial Commit

--- SendMail/Form1.Designer.vb
+++ SendMail/Form1.Designer.vb
@@ -22,9 +22,33 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(168, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Send"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(192, 51)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Send Email"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Button1 As Button
 End Class
--- SendMail/Form1.vb
+++ SendMail/Form1.vb
@@ -1,3 +1,30 @@
-﻿Public Class Form1
+﻿Imports System.Net.Mail
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Mail As New MailMessage
+        Mail.Subject = "test email"
+        Mail.To.Add("user@gmail.com")
+        Mail.From = New MailAddress("user@hotmail.com")
+        Mail.Body = "This is a test email using VB.NET " + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
+
+        'MessageBox.Show(Mail.Body)
+
+        'using Hotmail server
+        Dim SMTP As New SmtpClient("smtp.live.com")
+        SMTP.EnableSsl = True
+        SMTP.Credentials = New System.Net.NetworkCredential("user@hotmail.com", "password")
+        SMTP.Port = "587"
+        'SMTP.DeliveryMethod = SmtpDeliveryMethod.Network
+        Try
+            SMTP.Send(Mail)
+            MessageBox.Show("Sent", "Status", MessageBoxButtons.OK)
+        Catch ex As Exception
+            MessageBox.Show("Failed", "Status", MessageBoxButtons.OK)
+        End Try
+        Application.Exit()
+        End
+    End Sub
 
 End Class
